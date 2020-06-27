32ab79988485adaf0efba200a2e6d92b6abb832c
sidyadav2211 <kailashyadav1998@example.com>
2020-06-27 18:16:37 +0530

    Updates

---
+++ precache-manifest.898371da3e167eb18c995a4b8e8108ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d052725a52916a305b5bd84719c74663",
+    "url": "/demo-app-react/index.html"
+  },
+  {
+    "revision": "c082834535edbb5b7031",
+    "url": "/demo-app-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4460c702648efad5d646",
+    "url": "/demo-app-react/static/js/2.2713712b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-app-react/static/js/2.2713712b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c082834535edbb5b7031",
+    "url": "/demo-app-react/static/js/main.1e43ac57.chunk.js"
+  },
+  {
+    "revision": "81922fa7e8b0c28bba69",
+    "url": "/demo-app-react/static/js/runtime-main.13760810.js"
+  }
+]);
